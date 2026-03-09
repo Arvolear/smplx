fe796809b763b68bb4fdbcda476e13eb911a832e
Artem Chystiakov <artem.ch31@gmail.com>
2026-03-09 18:18:58 +0200

    example fix

--- examples/basic/tests/draft_test.rs
+++ examples/basic/tests/draft_test.rs
@@ -1,8 +1,6 @@
 use simplicityhl::elements::{Script, Txid};
 
 use simplex::simplex_sdk::constants::DUMMY_SIGNATURE;
-use simplex::simplex_sdk::provider::ProviderTrait;
-use simplex::simplex_sdk::signer::Signer;
 use simplex::simplex_sdk::transaction::{
     FinalTransaction, PartialInput, PartialOutput, ProgramInput, RequiredSignature,
 };
@@ -11,7 +9,9 @@ use simplex::simplex_sdk::utils::tr_unspendable_key;
 use draft_example::artifacts::p2pk::P2pkProgram;
 use draft_example::artifacts::p2pk::derived_p2pk::{P2pkArguments, P2pkWitness};
 
-fn get_p2pk(context: &simplex::TestContext, signer: &Signer) -> (P2pkProgram, Script) {
+fn get_p2pk(context: &simplex::TestContext) -> (P2pkProgram, Script) {
+    let signer = context.get_signer();
+
     let arguments = P2pkArguments {
         public_key: signer.get_schnorr_public_key().unwrap().serialize(),
     };
@@ -22,8 +22,11 @@ fn get_p2pk(context: &simplex::TestContext, signer: &Signer) -> (P2pkProgram, Sc
     (p2pk, p2pk_script)
 }
 
-fn spend_p2wpkh(context: &simplex::TestContext, signer: &Signer, provider: &dyn ProviderTrait) -> Txid {
-    let (_, p2pk_script) = get_p2pk(context, signer);
+fn spend_p2wpkh(context: &simplex::TestContext) -> Txid {
+    let signer = context.get_signer();
+    let provider = context.get_provider();
+
+    let (_, p2pk_script) = get_p2pk(context);
 
     let mut ft = FinalTransaction::new(*context.get_network());
 
@@ -41,8 +44,11 @@ fn spend_p2wpkh(context: &simplex::TestContext, signer: &Signer, provider: &dyn 
     res
 }
 
-fn spend_p2pk(context: &simplex::TestContext, signer: &Signer, provider: &dyn ProviderTrait) -> Txid {
-    let (p2pk, p2pk_script) = get_p2pk(context, signer);
+fn spend_p2pk(context: &simplex::TestContext) -> Txid {
+    let signer = context.get_signer();
+    let provider = context.get_provider();
+
+    let (p2pk, p2pk_script) = get_p2pk(context);
 
     let mut p2pk_utxos = provider.fetch_scripthash_utxos(&p2pk_script).unwrap();
 
@@ -71,16 +77,15 @@ fn spend_p2pk(context: &simplex::TestContext, signer: &Signer, provider: &dyn Pr
 
 #[simplex::test]
 fn dummy_test(context: simplex::TestContext) -> anyhow::Result<()> {
-    let signer = context.get_signer();
     let provider = context.get_provider();
 
-    let tx = spend_p2wpkh(&context, &signer, provider.as_ref());
+    let tx = spend_p2wpkh(&context);
 
     provider.wait(&tx)?;
 
     println!("Confirmed");
 
-    let tx = spend_p2pk(&context, &signer, provider.as_ref());
+    let tx = spend_p2pk(&context);
 
     provider.wait(&tx)?;
 
